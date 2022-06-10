320ab0b1c811a455c6d5c736df81eb9d1db95dcd
Rajesh Kumar  Jha <m1084848@mindtree.com>
2022-06-10 10:32:03 +0530

    login page started

--- src/main/java/com/example/pms/PatientmanagementsystemApplication.java
+++ src/main/java/com/example/pms/PatientmanagementsystemApplication.java
@@ -2,8 +2,10 @@ package com.example.pms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.example.pms")
 public class PatientmanagementsystemApplication {
 
 	public static void main(String[] args) {
